3f800077e1349aa4974d30f640e1d2a89a8a30bc
Le <117742888+hitaki1437@users.noreply.github.com>
2024-10-31 22:57:35 +0800

    update-16

--- removeHash.js
+++ removeHash.js
@@ -1,19 +1,3 @@
-$(document).ready(function() {
-  // get the anchor link buttons
-  const menuBtn = $('.hack15-menu-button');
-  // when each button is clicked
-  menuBtn.click(()=>{	
-    // set a short timeout before taking action
-    // so as to allow hash to be set
-    setTimeout(()=>{
-      // call removeHash function after set timeout
-      removeHash();
-    }, 5); // 5 millisecond timeout in this case
-  });
-
-  // removeHash function
-  // uses HTML5 history API to manipulate the location bar
-  function removeHash(){
-    history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
-  }
-});
+setTimeout(()=>{
+  history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
+}, 5); 
